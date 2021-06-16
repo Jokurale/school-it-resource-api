e84f05a6d5c12163a89d1cc0d1ff675f3870fbd4
jokurale <podsiadly.michal@protonmail.com>
2021-06-16 22:33:55 +0200

    Add check for already present attendance record - Actions performs checks for existing attendance record - if attendance record for given time and student id matches server refuses to insert another one

--- src/api/v1/services/AttendanceService.js
+++ src/api/v1/services/AttendanceService.js
@@ -6,12 +6,39 @@ const { ServiceFactory } = require("../factories");
 
 const factory = new ServiceFactory(prisma, validateAttendance, "attendance");
 
-const { getAll, getById, add, update, remove } = factory.getServiceFunctions();
+const { getAll, getById, update, remove } = factory.getServiceFunctions();
+
+const addAttendance = async (attendace) => {
+  const validAttendace = await validateAttendance(attendace);
+
+  // Check if exact same attendance record is present in database
+  const {
+    data: { date, studentId, hourId },
+  } = validAttendace;
+
+  const attendanceRecord = await prisma.attendance.findFirst({
+    where: {
+      date,
+      studentId,
+      hourId,
+    },
+  });
+
+  if (attendanceRecord)
+    throw Error(
+      "Attendance record for given time and student is already present. Please update existing one if needed."
+    );
+
+  // Procced if everything's ok
+  const result = await prisma.attendance.create(validAttendace);
+
+  return result;
+};
 
 module.exports = {
   getAllAttendances: getAll,
   getAttendanceById: getById,
-  addAttendance: add,
+  addAttendance,
   updateAttendance: update,
   removeAttendance: remove,
 };
